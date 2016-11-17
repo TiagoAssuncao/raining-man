b0db5abec3a180cc25af96cb026451bb2dcb1457
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-11-17 20:48:51 -0200

    Adding class to world

--- src/raining_man/world.py
+++ src/raining_man/world.py
@@ -1,14 +1,41 @@
 """
-	NOT USED YET.
+NOT USED YET.
 """
 
 from FGAme import *
+from boy import Boy
+from shot import Shot
 import pygame
 from pygame.locals import *
 
-class World:
+class RainingWorld(World):
+    def __init__(self):
+        super().__init__()
+        self.base_k = 1.05
+        self.recover_interval = 150
+        self.time_to_exaust = 200
+        self.spawn_time = 130
+        self.screen_end = -10
+        self.shot_list = []
+        self.count = 0
+        self.points = 0
+        self.is_exaust = False
 
-	def __init__(self):
-		self.main_character = list(objects)
-		self.list_objects = list(objects)
-		self.gravity = world.gravity(0, 30)
+    def add_raining_world(self):
+        """TODO: Docstring for add_raining_world.
+        :returns: nothing
+
+        create the objects to the rainging world
+
+        """
+
+        self.add.margin(200, -500)
+        self.gravity = (0, 30)
+
+        self.player = Boy()
+        self.player.body.gravity = (0, 0)
+        shot = Shot() # INITIAL SHOT
+        self.shot_list.append(shot)
+
+        pygame.mixer.pre_init(44100, 16, 2, 4096)
+        pygame.init()
