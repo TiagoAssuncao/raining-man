8f56821d0b592c906b17a2e00556478ff3b09367
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-12-06 19:49:30 -0200

    Adding class timer

--- src/raining_man/run.py
+++ src/raining_man/run.py
@@ -1,16 +1,6 @@
-from FGAme import *
-from math import sqrt
 from world import RainingWorld
-import pygame
-from pygame.locals import *
-from raining_man.shot import Shot, randomize
-import random
-import time
-import os
 
 world = RainingWorld()
 world.add_raining_world()
 world.start_sound()
-
-
 world.render_game()
--- src/raining_man/shot.py
+++ src/raining_man/shot.py
@@ -1,17 +1,11 @@
 from FGAme import *
-import os
-import pygame
 from core import Physics, Media
 
-
-
 class Shot(Circle):
-
     def __init__(self, pos=(400, 650)):
         super().__init__(radius=50,mass=10000, pos=pos, vel=(0, 100), 
                          color='brown')
         self.body = world.add(self)
-
         self.body_pygame = Media.change_image('images/stone.png')
         self.k = 1.05
 
